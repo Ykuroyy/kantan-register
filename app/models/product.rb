dee0b1cd5df89ef0619257ab4b7b3f829f1a42ff
Ykuroyy <yykm666@gmail.com>
2025-05-18 18:58:08 +0900

    単体テスト

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,12 @@ class Product < ApplicationRecord
   has_one_attached :image
   has_many :order_items
 
-  validates :name, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true, length: { maximum: 40 }
+
+  validates :price, presence: true,
+                    numericality: {
+                      only_integer: true,
+                      greater_than: 0,
+                      less_than_or_equal_to: 9_999_999
+                    }
 end
---
+++ spec/factories/products.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :product do
+    name { "テスト商品" }
+    price { 1000 }
+
+  end
+end
