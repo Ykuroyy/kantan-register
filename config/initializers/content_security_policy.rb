64ba0c981b621ae80d4d0cb0a0160fd167ec9bfb
Ykuroyy <yykm666@gmail.com>
2025-05-25 09:14:06 +0900

    css追記

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,18 +4,28 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-Rails.application.configure do
-  config.content_security_policy do |policy|
-    policy.default_src :self, :https
-    policy.font_src    :self, :https, "https://fonts.gstatic.com"
-    policy.img_src     :self, :https, :data
-    policy.object_src  :none
-    policy.script_src  :self, :https
-    policy.style_src   :self, :https, "https://fonts.googleapis.com"
-     # Specify URI for violation reports
-    # policy.report_uri "/csp-violation-report-endpoint"
-  end
-#
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  policy.script_src  :self, :https
+  policy.style_src   :self, :https, :unsafe_inline # Tailwind/Vite なら :unsafe_inline が必要
+  # ↑ 追加ルールがあればここに書く
+end
+
+# --- 追加設定（必要なときだけアンコメント） --------------------------
+
+# Rails.application.config.content_security_policy_nonce_generator =
+#   ->(_request) { SecureRandom.base64(16) }
+
+# Rails.application.config.content_security_policy_nonce_directives =
+#   %w(script-src style-src)
+
+# Rails.application.config.content_security_policy_report_only = true
+
 #   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
 #   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
 #   config.content_security_policy_nonce_directives = %w(script-src style-src)
